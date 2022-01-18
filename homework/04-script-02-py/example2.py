952e5164a7d8453948d370a4c44bd7c0bde4db4e
mike <mike.bogdanov@inbox.ru>
2022-01-18 12:40:17 +0800

    added homework_04-script-02-py

--- homework/04-script-02-py/example2.py
+++ homework/04-script-02-py/example2.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python3
 
 import os
+#добавляем для работы с параметрами в командной строке
 import sys
 
+#проверяем наличие параметра в команде (т.е. длина команды больше или равна 2, т.к. по умолчанию без параметра длина 1)
+#при этом перенаправляем вывод ошибок git status в &1, чтобы не мешало в дальнейшем
 if len(sys.argv)>=2:
-    bash_command = ["cd "+sys.argv[1], "git status"]
+        bash_command = ["cd "+sys.argv[1], "git status 2>&1"]
 else:
-    bash_command = ["git status"]
+        bash_command = ["git status 2>&1"]
 
 result_os = os.popen(' && '.join(bash_command)).read()
 
 for result in result_os.split('\n'):
-    if result.find('modified') != -1:
-        prepare_result = os.getcwd()+'/'+result.replace('\tmodified:   ', '')
-        print(prepare_result)
-
+        #проверяем, есть ли git в этой папке
+        if result.find('fatal') != -1:
+                print('В папке '+sys.argv[1]+' нет GIT репозитория')
+        #вывод списка измененных файлов
+        if result.find('modified') != -1:
+                prepare_result = os.getcwd()+'/'+result.replace('\tmodified:   ', '')
+                print(prepare_result)
